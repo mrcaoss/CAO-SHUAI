6b474c5bdf6251a5cffe60dbe96ce7d9efc202e5
ppx <1228857161@qq.com>
2020-09-08 20:49:28 +0800

    补充

---
+++ big/neirong/js/login.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#link-login').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link-reg').on('click', function () {
+        $('.login.box').show()
+        $('.reg-box').hide()
+    })
+    layuui.form.verify({
+        password: [/^[\s]{6,12}$/, '密码必须6到12位'],
+        repassword: function (value) {
+            if ($('#reg-psd').val() !== value) {
+                alert("密码都输不对，")
+                return '密码不一样'
+            }
+        },
+    })
+    // 绑定submit\
+    $('.layui-form').submit(function (e) {
+        e.preventDefault()
+        var username = $('#reg-username').val()
+        var password = $('#reg-psd').val()
+        var formdata = {
+            username: username,
+            password: password,
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', formdata, function (res) {
+            if (res.status == 0) {
+                console.log(res.message);
+            } else {
+                console.log(res.message);
+            }
+            alert(1)
+        })
+    })
+})
